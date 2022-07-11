5d21b4bb3bec4f1b5f968d73d2a58a662c0df58b
Park-Seaweed <qkralsgur721@gmail.com>
2022-07-11 18:58:47 +0900

    api 호출

--- app.py
+++ app.py
@@ -1,7 +1,26 @@
-from flask import Flask, render_template
+from urllib import response
+from wsgiref import headers
+from pymongo import MongoClient
+from flask import Flask, render_template, jsonify, request
+import requests
+from pprint import pprint
 
 
 app = Flask(__name__)
+
+
+url = 'https://api.odcloud.kr/api/15042291/v1/uddi:f831fb62-a6ca-483b-94f8-dfc2243fa3ef'
+
+params = {
+        'page' : '1',
+        'perPage': '135',
+        'serviceKey': 'Gt8j+nZ9SiGh7agtjfQ7HXTDG9MQf7CXibre+C8J7Sk5vb9fi6rR7Z6RDZMxpNp0w+mDWgeGf/Cy5At3GNZQ1g=='
+}
+
+response = requests.get(url, params=params)
+data = response.json()
+pprint(data)
+    
 
 
 @app.route('/')
@@ -11,7 +30,13 @@ def main():
 
 @app.route('/sign_up')
 def detail():
-    return render_template("detail.html")
+    return render_template("sign_up.html")
+
+
+@app.route('/alcohol_info')
+def alcohol_info():
+    return render_template("alcohol_info")
+
 
 
 if __name__ == '__main__':
