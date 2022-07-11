b4632c82a959f7f61d155a9c83390bbdca58f955
Park-Seaweed <qkralsgur721@gmail.com>
2022-07-12 01:57:22 +0900

    api db저장 컨트롤

--- app.py
+++ app.py
@@ -1,26 +1,54 @@
+from http import client
 from urllib import response
 from wsgiref import headers
 from pymongo import MongoClient
 from flask import Flask, render_template, jsonify, request
 import requests
-from pprint import pprint
-
+import json
 
 app = Flask(__name__)
 
+client = MongoClient('mongodb+srv://test:sparta@cluster0.n802d.mongodb.net/?retryWrites=true&w=majority')
+db = client.dbsparta
 
-url = 'https://api.odcloud.kr/api/15042291/v1/uddi:f831fb62-a6ca-483b-94f8-dfc2243fa3ef'
-
-params = {
+@app.route('/aaa', methods=["POST"])
+def post_now_showing():
+    url = 'https://api.odcloud.kr/api/15042291/v1/uddi:f831fb62-a6ca-483b-94f8-dfc2243fa3ef'
+    headers = {
+        'Authorization' : 'uO1FqnqYYWxvHGeughWph2r0W6+bqIPqgtC4eqiJ1sWInFsnyy7t3zLr4ZrVxum49BK+qA0xDxRWBMmYZake3w=='
+    }
+    params = {
         'page' : '1',
         'perPage': '135',
-        'serviceKey': 'Gt8j+nZ9SiGh7agtjfQ7HXTDG9MQf7CXibre+C8J7Sk5vb9fi6rR7Z6RDZMxpNp0w+mDWgeGf/Cy5At3GNZQ1g=='
-}
-
-response = requests.get(url, params=params)
-data = response.json()
-pprint(data)
+        'serviceKey': 'uO1FqnqYYWxvHGeughWph2r0W6+bqIPqgtC4eqiJ1sWInFsnyy7t3zLr4ZrVxum49BK+qA0xDxRWBMmYZake3w=='
+    }
+    alcoholList = requests.get(url, headers=headers, params=params).json()
+    alcohol_infos = alcoholList['data']
     
+    
+    db.alchols.delete_one({
+            'place': "place",
+            'name': "name",
+            'kind': "kind"
+        })
+    
+    for alchol in alcohol_infos:
+        place = alchol["주소"]
+        name = alchol["품명"]
+        kind = alchol["품목"]
+        doc = {
+            'place': place,
+            'name': name,
+            'kind': kind
+        }
+        db.alchols.insert_one(doc)
+
+    return jsonify({'result': 'success'})
+
+
+# @app.route('/aaa', methods=['GET'])
+# def get_now_showing():
+#     return
 
 
 @app.route('/')
