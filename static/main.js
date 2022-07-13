846559603b3cdeeeb8077116eae26907286847b6
oh <outputor777@gmail.com>
2022-07-14 01:53:00 +0900

    지도 움직이는 기능 업데이트

--- static/main.js
+++
@@ -1,6 +0,0 @@
-// const map = document.querySelector('#map');
-// console.log(map);
-// document.addEventListener('scroll', (e) => {
-//     let scrollY = e.target.scrollingElement.scrollTop;
-//     if(scrollY >= 200)
-// })
