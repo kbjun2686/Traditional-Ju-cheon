bbd041184d811324d4ba6c5449b81571b403d849
oh <outputor777@gmail.com>
2022-07-13 22:44:22 +0900

    지도 클릭 이벤트 업데이트

---
+++ static/main.js
@@ -0,0 +1,6 @@
+// const map = document.querySelector('#map');
+// console.log(map);
+// document.addEventListener('scroll', (e) => {
+//     let scrollY = e.target.scrollingElement.scrollTop;
+//     if(scrollY >= 200)
+// })
